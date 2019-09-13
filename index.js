1f6563a25029a7dccb1ccd1e75ae19b3f2c6d80a
t-s-t <55143758+t-s-t@users.noreply.github.com>
2019-09-13 23:09:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const blessed = require('blessed');
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
 const token = '';
+
 
 var screen = blessed.screen({
   smartCSR: true,
@@ -13,84 +15,73 @@ screen.key(['q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
 
+
+var guildsMenu = blessed.Listbar({
+  height : '5%',
+  keys: true,
+  border: 'line',
+  style : {
+    fg: 'white',
+    border: {fg: '#ffffff'},
+    selected: {bg: 'green'}
+  },
+});
+
+
+//make pretty
 function guildCallback(guild) {
   channelMenu.setItems([]);
   channelMenuArray = [];
-  guild.channels.forEach( function(channel) {
-    channelMenu.add(channel.name);
+  var sortedChannels = guild.channels.sort((a, b) => a.position - b.position);
+  sortedChannels.forEach( function(channel) {
+    if (channel.type == 'category'){
+      channelMenu.add('>'+channel.name);
+    }else{
+      channelMenu.add(channel.name);
+    }
     channelMenuArray.push(channel);
     });
   channelMenu.focus();
+  //console.log(guild.channels);
   screen.render();
 }
+
+function appendGuild(guild) {
+  guildsMenu.add(guild.name, () => {guildCallback(guild)});
+}
+
 
 var chatMenu = blessed.List({
   left: '10%-1',
   top: '5%-1',
   keys: true,
   border: 'line',
-});
-
-var guildsMenu = blessed.Listbar({
-  height : '5%',
-  //left: '10%',
-  keys: true,
-  border: 'line',
   style : {
     fg: 'white',
-    //bg: 'magenta',
-    border: {
-      fg: '#ffffff'
-    },
-    selected: {
-      bg: 'green'
-    }
+    border: {fg: '#ffffff'},
+    selected: {bg: 'green'}
   },
+});
 
-}
-);
-
-
-// guildsMenu.key('left', () => {
-//   guildsMenu.moveLeft(1);
-//   screen.render();
-// });
 
 var channelMenu = blessed.List({
-  parent: guildsMenu,
   keys: true,
   top: '5%-1',
   width: '10%',
   border: 'line',
   style : {
     fg: 'white',
-    //bg: 'magenta',
-    border: {
-      fg: '#ffffff'
-    },
-    selected: {
-      bg: 'green'
-    }
+    border: {fg: '#ffffff'},
+    selected: {bg: 'green'}
   },
 });
 
 var channelMenuArray = [];
 
-// guildsMenu.key('right', () => {
-//   guildsMenu.moveRight(1);
-//   screen.render();
-// });
-
-function appendGuild(guild) {
-  guildsMenu.add(guild.name, () => {guildCallback(guild)});
-}
-
-function findGuild(name) {
-  for (var i = 0; i < client.guilds.length; i++) {
-   if (client.guilds[i].name == name) {
-     return i;
-   }
- }   return None;
+function formatChannels(channels) {
+  var stringArray = [];
+  channels.sort((a, b) => parseFloat(a.position) - parseFloat(b.position));
+  //console.log(channels);
 }
 
 client.on('ready', () => {
@@ -99,13 +90,24 @@ client.on('ready', () => {
   screen.render();
 });
 
+function appendChat(messages) {
+  messages.forEach( message => {
+    chatMenu.add(message.content);
+  });
+  chatMenu.focus();
+  screen.render();
+}
+
 channelMenu.on('select', () => {
-  channelMenuArray[channelMenu.selected].fetchMessages({limit:20})
-  .then(messages => {messages.forEach( function(msg) {chatMenu.add(msg.author.username+' | '+msg.content)})});
+  chatMenu.setItems([]);
+  channelMenuArray[channelMenu.selected].fetchMessages({ limit: 50 })
+  .then(messages => appendChat(messages))
+  .catch(console.error);
   screen.render();
 });
 
 channelMenu.key('escape', () => {guildsMenu.focus();});
+chatMenu.key('escape', () => {channelMenu.focus();});
 
 screen.append(guildsMenu);
 screen.append(channelMenu);
@@ -116,3 +118,4 @@ screen.render();
 guildsMenu.focus();
 
 client.login(token);
+
